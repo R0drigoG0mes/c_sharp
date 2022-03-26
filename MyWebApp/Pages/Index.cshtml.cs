ccb5c345e1994f3e2858d7659a1d5fff182783ca
Rodrigo Gomes Cordeiro <98043205+R0drigoG0mes@users.noreply.github.com>
2022-03-25 21:12:50 -0300

    Estudos do dia 25/03/2022

---
+++ MyWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Windows;
+
+namespace MyWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public int le = 9;
+    private readonly ILogger<IndexModel> _logger;
+
+
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
